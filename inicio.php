700f148e6ccc1de06c27015981530a7ae594afc9
Vitor Freitas <vitorfs@gmail.com>
2014-07-17 15:45:12 -0300

    add imagens cursos

--- inicio.php
+++ inicio.php
@@ -64,19 +64,19 @@
     <div class="container marketing">
       <div class="row">
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+          <img src="imgs/java.png" alt="Java" style="height: 140px;">
           <h2>Programação Java para Web</h2>
           <p>Voltado para o desenvolvimento de sites dinâmicos utilizando a linguagem de programação Java.</p>
           <p><a class="btn btn-default" href="cursos/java_web.php" role="button">Mais detalhes »</a></p>
         </div>
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+          <img src="imgs/android.png" alt="Android" style="height: 140px;">
           <h2>Desenvolvimento de aplicações Android</h2>
           <p>Voltado para o desenvolvimento de aplicações para celulares e tablets na plataforma Android.</p>
           <p><a class="btn btn-default" href="#" role="button">Mais detalhes »</a></p>
         </div>
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+          <img src="imgs/html5.png" alt="HTML 5" style="height: 140px;">
           <h2>JQuery & HTML 5</h2>
           <p>Voltado para o desenvolvimento de layouts de páginas web utilizando JQuery e HTML 5.</p>
           <p><a class="btn btn-default" href="#" role="button">Mais detalhes »</a></p>
