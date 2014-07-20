9b142bfa9f04fb9d3d75b6fb03c3e9b74521fee1
bastosbf <bastosbf@gmail.com>
2014-07-20 19:05:26 -0300

    Adicionando o Sobre.

--- sobre.php
+++ sobre.php
@@ -1,18 +1,52 @@
 <?php include("topo.php"); ?>
 
 <div class="container">
-  <h1 class="page-header">Sobre</h1>
-  <h4>
-    A empresa BFTec visa formar profissionais qualificados para o desenvolvimento de Software
-    em diferentes tecnologias. Os cursos oferecidos pela BFTec são voltados para empresas que 
-    desejam treinar seus funcionários e podem ser adaptados de acordo com as ferramentas utilizadas na empresa.
-    <br /><br />
-    Os cursos podem ser montados utilizando as tecnologias disponíveis em nosso site, de acordo com a necessidade de cada empresa.
-    Para montar seus curso entre em contato conosco através do próprio site.
-    <br /><br />
-    Os cursos oferecidos por nossa empresa são ministrados por profissionais qualificados que já tiveram experiência com essas
-    tecnologias no mercado de trabalho.
-  </h4>
+	<h1 class="page-header">Sobre</h1>
+	<h4>
+		A empresa BFTec visa formar profissionais qualificados para o
+		desenvolvimento de Software em diferentes tecnologias. Os cursos
+		oferecidos pela BFTec são voltados para empresas que desejam treinar
+		seus funcionários e podem ser adaptados de acordo com as ferramentas
+		utilizadas na empresa. <br /> <br /> Os cursos podem ser montados
+		utilizando as tecnologias disponíveis em nosso site, de acordo com a
+		necessidade de cada empresa. Para montar seus cursos entre em contato
+		conosco através do próprio site. <br /> <br /> Os cursos oferecidos
+		por nossa empresa são ministrados por profissionais qualificados que
+		já tiveram experiência com essas tecnologias no mercado de trabalho.
+	</h4>
+	<hr class="featurette-divider">
+	<div class="row featurette">
+		<div class="col-md-7">
+			<h2 class="featurette-heading">Bruno Bastos</h2>
+			<p class="lead">Formado na Universidade Estácio de Sá, Bruno Bastos é
+				analista de sistemas do Sistema Nacional de Processamento de Alto
+				Desempenho e aluno de mestrado da Universidade Federal de Juiz de
+				Fora. Atua no desenvolvimento de portais, utilizando a linguagem de
+				programação Java e seus diversos frameworks. É desenvolvedor de
+				sistemas desde 2005, e já atuou no desenvolvimento de aplicações
+				desktop, web e mobile, utilizando diversas tecnologias.</p>
+		</div>
+		<div class="col-md-5">
+			<img class="featurette-image img-responsive"
+				data-src="holder.js/500x500/auto" alt="500x500"
+				src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjI1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjMxcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NTAweDUwMDwvdGV4dD48L3N2Zz4=">
+		</div>
+	</div>
+	<hr class="featurette-divider">
+	<div class="row featurette">
+		<div class="col-md-5">
+			<img class="featurette-image img-responsive"
+				data-src="holder.js/500x500/auto" alt="500x500"
+				src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjI1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjMxcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NTAweDUwMDwvdGV4dD48L3N2Zz4=">
+		</div>
+		<div class="col-md-7">
+			<h2 class="featurette-heading">
+				Vitor Freitas</span>
+			</h2>
+			<p class="lead"></p>
+		</div>
+	</div>
+
 </div>
 
 <?php include("rodape.php"); ?>
