3526afaf43192812fe81904696fc5bc4f87c38ea
bastosbf <bastosbf@gmail.com>
2014-09-04 20:25:24 -0300

    adicionando css 3 e html 5

--- tecnologias/html5_css3.php
+++ tecnologias/html5_css3.php
@@ -4,11 +4,26 @@
 	<h1 class="page-header">HTML 5 & CSS 3</h1>
 	<ul>
 		<li><h3>Introdução ao HTML</h3></li>
-		<li><h3>O HTML 5</h3></li>
+		<li><h3>Conceitos Básicos do HTML 5</h3></li>
+		<ul>
+			<li><h4>Criando o Documento</h4></li>
+			<li><h4>Formulários</h4></li>			
+		</ul>
 		<li><h3>Rich Media</h3></li>
-		<li><h3>Formulários</h3></li>
 		<li><h3>Semântica do HTML 5</h3></li>
 		<li><h3>Usando HTML 5</h3></li>
+		<li><h3>Introdução ao CSS</h3></li>
+		<li><h3>Conceitos Básicos do CSS 3</h3></li>
+		<ul>
+			<li><h4>Utilizando Estilos</h4></li>
+			<li><h4>Acessando Propriedades de Fontes, Textos e Cores</h4></li>			
+		</ul>
+		<li><h3>Criando sua Interface com CSS</h3></li>
+		<ul>
+			<li><h4>Modificando Componentes HTML</h4></li>
+			<li><h4>Classes CSS</h4></li>			
+		</ul>
+		<li><h3>Melhores Práticas de Uso</h3></li>
 	</ul>
 </div>
 
