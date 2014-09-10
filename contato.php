c7ae14c4c47b5da36ae19758c853a3fe552251f7
bastosbf <bastosbf@pc8812>
2014-09-10 09:45:47 -0300

    adicionando curso de html e modificando formulário de contato

--- contato.php
+++ contato.php
@@ -1,49 +1,26 @@
 <?php include("topo.php"); ?>
 
 <div class="container">
-  <h1 class="page-header">Contato</h1>
-  <div class="row">
-    <div class="col-md-12">
-      <?php
-        if ($_POST[sucesso] == 1) {
-          echo '<div class="alert alert-success"><strong><span class="glyphicon glyphicon-send"></span> Mensagem enviada com sucesso!</strong></div>';
-        }
-        if ($_POST[erro] == 1) {   
-          echo '<div class="alert alert-danger"><span class="glyphicon glyphicon-alert"></span><strong> Erro! Por favor verifique os dados informados...</strong></div>';
-        }
-      ?>
-    </div>
-    <div class="col-lg-6">
-      <form role="form" action="enviar_email.php" method="post" >
-        <div class="form-group">
-          <label for="nome">Nome</label>
-          <div class="input-group">
-            <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite seu nome" required>
-            <span class="input-group-addon"><i class="form-control-feedback"></i></span>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="email">E-mail</label>
-          <div class="input-group">
-            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail" required  >
-            <span class="input-group-addon"><i class="form-control-feedback"></i></span>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="msg">Mensagem</label>
-          <div class="input-group">
-            <textarea name="msg" id="msg" class="form-control" rows="5" required></textarea>
-            <span class="input-group-addon"><i class="form-control-feedback"></i></span>
-          </div>
-        </div>
-        <input type="submit" name="submit" id="submit" value="Enviar" class="btn btn-info pull-right">
-      </form>
-    </div>
-    <div class="col-lg-6" align="justify">
-      <h3>Entre em contato conosco para saber mais informações sobre nossos cursos.</h3>
-    </div>
-    <hr class="featurette-divider hidden-lg">
-  </div>
+	<h1 class="page-header">Contato</h1>
+	<div class="row">
+		<div class="col-lg-12" align="justify">
+			<h3>Entre em contato conosco através do e-mail abaixo para saber mais
+				informações sobre nossos cursos.</h3>
+			<br />
+			<div align="center">
+				<h2>
+				<script>document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'&'+'#'+'1'+'0'+'9'+';'+'&'+'#'+'9'+'7'+';'+'i'+'l'+'t'+'o'+'&'+
+'#'+'5'+'8'+';'+'c'+'o'+'n'+'t'+'&'+'#'+'9'+'7'+';'+'t'+'o'+'&'+'#'+'6'+'4'+';'+'&'+'#'+'9'+'8'+';'+
+'f'+'t'+'e'+'c'+'&'+'#'+'4'+'6'+';'+'&'+'#'+'1'+'1'+'0'+';'+'%'+'6'+'&'+'#'+'5'+'3'+';'+'%'+'7'+'&'+
+'#'+'5'+'2'+';'+"'"+'>'+'c'+'&'+'#'+'1'+'1'+'1'+';'+'n'+'&'+'#'+'1'+'1'+'6'+';'+'a'+'&'+'#'+'1'+'1'+
+'6'+';'+'o'+'&'+'#'+'6'+'4'+';'+'b'+'f'+'t'+'e'+'c'+'&'+'#'+'4'+'6'+';'+'n'+'e'+'t'+'<'+'/'+'a'+'>'
+);</script>
+				</h2>
+			</div>
+		</div>
+
+		<hr class="featurette-divider hidden-lg">
+	</div>
 </div>
 
 <?php include("rodape.php"); ?>
--- cursos/jquery_html5_css3.php
+++ cursos/jquery_html5_css3.php
@@ -2,6 +2,78 @@
 
 <div class="container">
 	<h1 class="page-header">jQuery, HTML 5 & CSS 3</h1>
+	<div align="left">
+		<h2>
+			<b>Parte I</b>
+		</h2>
+	</div>
+	<ul>
+		<li><h3>Introdução ao HTML</h3></li>
+		<ul>
+			<li><h4>O que é HTML</h4></li>
+			<li><h4>O que é o protocolo HTTP</h4></li>
+			<li><h4>Conceitos básicos da linguagem HTML</h4></li>
+		</ul>
+		<li><h3>Introdução ao CSS</h3></li>
+		<ul>
+			<li><h4>EStilos em uma página HTML</h4></li>
+		</ul>
+		<li><h3>Introdução ao Javascript</h3></li>
+		<ul>
+			<li><h4>Criação de funções</h4></li>
+			<li><h4>Tratamento de eventos</h4></li>
+		</ul>		
+	</ul>
+	<div align="left">
+		<h2>
+			<b>Parte II</b>
+		</h2>
+	</div>
+	<ul>
+		<li><h3>HTML 5</h3></li>
+		<ul>
+			<li><h4>Criando o Documento</h4></li>
+			<li><h4>Formulários</h4></li>			
+		</ul>
+		<li><h3>Rich Media</h3></li>
+		<li><h3>Semântica do HTML 5</h3></li>			
+		<ul>
+			<li><h4>Utilizando Estilos</h4></li>
+			<li><h4>Acessando Propriedades de Fontes, Textos e Cores</h4></li>			
+		</ul>
+		<li><h3>Criando sua Interface com CSS 3</h3></li>
+		<ul>
+			<li><h4>Modificando Componentes HTML</h4></li>
+			<li><h4>Classes CSS</h4></li>			
+		</ul>
+		<li><h3>Fundamentos JQuery</h3></li>
+		<ul>
+			<li><h4>Habilitando JQuery em seu site</h4></li>
+			<li><h4>Adicionando códigos JQuery</h4></li>
+		</ul>
+		<li><h3>Seletores jQuery</h3></li>
+		<ul>
+			<li><h4>Usando a função "$()"</h4></li>
+			<li><h4>Selecionando elementos, atributos e estilos</h4></li>
+		</ul>
+		<li><h3>Manipulando conteúdo</h3></li>
+		<ul>
+			<li><h4>Modificando estilos</h4></li>
+			<li><h4>Manipulando eventos</h4></li>
+		</ul>		
+	</ul>
+	<div align="left">
+		<h2>
+			<b>Parte III</b>
+		</h2>
+	</div>
+	<ul>
+		<li><h3>Criando uma página HTML responsiva</h3></li>
+		<ul>
+			<li><h4>Mudando estilos</h4></li>
+			<li><h4>Tratando eventos</h4></li>
+			<li><h4>Acesso através de um navegador ou dispositivo móvel</h4></li>
+		</ul>	
+	</ul>		
 </div>
-
 <?php include("rodape.php"); ?>
